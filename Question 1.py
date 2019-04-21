e142481a26e6f36dc981b3a37f6c49dae0f5a2ab
OSAMABin <sp19bsse0033@MAJU.EDU.PK>
2019-04-21 22:11:15 +0500

    done

---
+++ Question 1.py
@@ -0,0 +1,12 @@
+print("Number Analyser")
+
+num = float(input("Enter a number:"))
+
+if num > 0:
+    print("positive number")
+
+elif num == 0:
+    print("Negative number")
+
+else:
+    print("Zero")
