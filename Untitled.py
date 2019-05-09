f16656c5eb8c5b818b38e82bd8b35a9f6a95a881
OSAMABin <49777730+OSAMABin@users.noreply.github.com>
2019-05-10 01:08:31 +0500

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import matplotlib.pyplot as plt
+x=[1,3,4,5,10,56,25,45]
+y=[2,4,5,6,8,10,45,85]
+plt.plot(x,y)
+plt.show()
+
+
+# In[4]:
+
+import matplotlib.pyplot as plt
+x=[1,3,5,7,9,11,13,15]
+y=[2,4,5,6,8,10,45,85]
+plt.bar(x,y)
+plt.show()
+
+
+# In[8]:
+
+import matplotlib.pyplot as plt
+x=["A","B","C","D"]
+y=[2,4,5,6,8,10,45,85]
+plt.bar(x,y)
+plt.show
+
