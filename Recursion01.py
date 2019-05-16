783f5bbb542aafd05b17cc5099ef32ce85d4f30b
OSAMABin <49777730+OSAMABin@users.noreply.github.com>
2019-05-17 00:48:52 +0500

    Recursion01

---
+++ Recursion01.py
@@ -0,0 +1,13 @@
+# Recursion
+
+# This Program has a recursion function.
+
+def main():
+    message()
+
+def message():
+    print("This is a recursion function")
+    message()
+
+# Call the main function.
+main()
