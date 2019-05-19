2efb349e10493214ba88a59f6f14e833660ef546
OSAMABin <49777730+OSAMABin@users.noreply.github.com>
2019-05-19 23:30:04 +0500

    Add files via upload

---
+++ emptywindow.py
@@ -0,0 +1,17 @@
+# enpty_window1.py
+
+#This program display an empty window.
+
+import tkinter
+
+def main():
+    #create the main window widget.
+    main_window = tkinter.Tk()
+
+
+    # Enter the tkinter main loop.
+    tkinter.mainloop()
+
+
+# Call the main function.
+main()
