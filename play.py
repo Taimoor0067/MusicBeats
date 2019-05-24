6449e2075fcb1c3ac4e93bd0fb6441ec4ee00b35
Rimsha0028 <49788760+Rimsha0028@users.noreply.github.com>
2019-05-24 18:39:47 +0500

    Add files via upload

---
+++ play.py
@@ -0,0 +1,20 @@
+def play_music():
+    global paused
+
+    if paused:
+        mixer.music.unpause()
+        statusbar['text'] = "Music Resumed"
+        paused = FALSE
+    else:
+        try:
+            stop_music()
+            time.sleep(1)
+            selected_song = playlistbox.curselection()
+            selected_song = int(selected_song[0])
+            play_it = playlist[selected_song]
+            mixer.music.load(play_it)
+            mixer.music.play()
+            statusbar['text'] = "Playing music" + ' - ' + os.path.basename(play_it)
+            show_details(play_it)
+        except:
+            tkinter.messagebox.showerror('File not found', 'Music Beats could not find the file. Please check again.')
